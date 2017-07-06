42506145d64bbe51c2797e8e28a91171839ab9a3
husseinmaad <hussein.maad@outlook.com>
2017-07-06 04:05:16 -0700

    second version

--- js/homepage.js
+++ js/homepage.js
@@ -2,21 +2,114 @@
   $( document ).ready(function() {
     
     var selected_button 
+    
+
     $(".btn-group").on('click', function(e){
       e.preventDefault();
        console.log(e.target.value)
-       selected_button = e.target.value;
+      selected_button = e.target.value;
+      $('.card').hide();
+      $(selected_button).fadeTo(400, 1);
+      $('.navbar-fixed-bottom').show();
     })
 
     $('#apply').on('click',function(e){
       e.preventDefault();
-
-      console.log($(".bed_2"));
-      $('.card').hide();
-      $(selected_button).show();
     })
 
     $('#cancle').on('click',function(e){
 
     })
+
+
+  
+
+  $(function () {
+                $('#datetimepicker1').datepicker();
+            });
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 400,
+    max: 4000,
+    values: [ 400, 4000 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).text( ui.values[ 0 ] );
+      $( "#amount2" ).text( ui.values[ 1 ] );
+      $('#test').val( ui.values[ 1 ] )
+    }
+  });
+
+  $( "#slider-range1" ).slider({
+    range: true,
+    min: 400,
+    max: 4000,
+    values: [ 400, 4000 ],
+    slide: function( event, ui ) {
+      $( "#amo1" ).text( ui.values[ 0 ] );
+      $( "#amo2" ).text( ui.values[ 1 ] );
+
+    }
+  });
+
+  $("#amount").change(function() {
+
+    $("#slider-range").slider('values',0,$(this).val());
+  });
+
+  $("#amount2").change(function() {
+      $("#slider-range").slider('values',1,$(this).val());
+  });
+
+
+  $('nav').fadeTo(100,0.9)
+
+  $("span").mouseup(function(){
+    var components = $('.container').find('.row').find('.card')
+    var min = Number($( "#amount" ).text());
+    var max = Number($( "#amount2" ).text());
+    var arr_card = []
+    filter_check(components)
+      for(var i =0; i<components.length; i++){ 
+
+        var priceElement = $(components[i]).find('bdi')
+        var currency = Number(priceElement[0].innerText.replace(/[^0-9\.]+/g,""));
+
+      if (currency >= min && currency <= max) {
+        arr_card.push(components[i]);
+      }
+    }
+    for (var i =0;i < arr_card.length; i++) {
+     $(arr_card[i]).show();
+    }
+  });
+
+more_less_check()
+
 });
+
+var filter_check = function(input){
+  for (var i =0;i < input.length; i++) {
+   $(input[i]).hide()
+   }
+}
+
+var more_less_check = function(){
+  var icon = '<i class="fa fa-chevron-down" aria-hidden="true"></i>'
+  $('#more').on('click',function(){
+    var link = $('#more')
+    if ($('#more')[0].innerText == " More") {
+      
+      $('.less').append(link.html('<i class="fa fa-chevron-up" aria-hidden="true"></i>' + " Less"))
+       
+    }
+    else {
+      var temp2 = $('#carpet').parent()[0]
+      $(temp2).append(link.html('<i class="fa fa-chevron-down" aria-hidden="true"></i>' + " More"))
+
+      
+    }
+  })
+}
+
+
+
